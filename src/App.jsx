2c9a654da0812ad220a6a257242dd4a90286f9ef
TemirovaAzima <temirovaazima9@gmail.com>
2025-02-08 22:38:10 +0500

    useRef with previous state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import {useState, useRef, useEffect} from 'react';
+import {useState, useRef,useEffect} from 'react'
 export const App = ()=>{
-    const [stateCount, setStateCount] = useState(0);
-    const countRef = useRef(0);
+    const [count, setCount] = useState(0);
+    const prevCountRef = useRef(null);
 
     useEffect(()=>{
-        console.log("Component re-rendered!")
-
-    })
-    const incrementRef = ()=>{
-        countRef.current+=1;
-        // the reason u can see the updated result after pressing increase state is that because
-        // during re-render component reads the current value of countRef
-        console.log("countRef:", countRef.current);
-    }
-
-    const incrementState = ()=>{
-        setStateCount(stateCount+1)
-    }
-    return(
+        prevCountRef.current = count;
+    },[count]);
+    return (
         <div>
-            {console.log("Rendering")}
-            <p>State Count : {stateCount}</p>
-            <p>countRef (Cant see) : {countRef.current}</p>
-            <button onClick={incrementRef}>increase Ref</button>
-            <button onClick={incrementState}>increase State</button>
+            <p>Current Count : {count}</p>
+            <p>Previous Count : {prevCountRef.current}</p>
+            <button onClick={()=>setCount(count+1)}>Increase</button>
         </div>
     )
 }
