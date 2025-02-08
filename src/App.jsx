1ae09a5f685e9aef5c84a2a30b4443f1ff2b1c69
TemirovaAzima <temirovaazima9@gmail.com>
2025-02-08 23:01:37 +0500

    useRef with setInterval end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import {useState, useRef,useEffect} from 'react'
-export const App = ()=>{
+import {useState, useRef, useEffect} from 'react';
+
+export default function App (){
     const [count, setCount] = useState(0);
-    const prevCountRef = useRef(null);
+    const intervalRef = useRef(null);
 
     useEffect(()=>{
-        prevCountRef.current = count;
-    },[count]);
+        intervalRef.current = setInterval(()=>{
+            setCount((prevCount)=> prevCount + 1)
+        },1000);
+        return(()=> {clearInterval(intervalRef.current)})
+    },[]);
+
     return (
         <div>
-            <p>Current Count : {count}</p>
-            <p>Previous Count : {prevCountRef.current}</p>
-            <button onClick={()=>setCount(count+1)}>Increase</button>
+            <h2>Count : {count}</h2>
+            <button onClick={()=>clearInterval(intervalRef.current)}>Stop</button>
         </div>
-    )
-}
+    );
+};
