c12dd21a52771651773f368a7f741580127494a6
TemirovaAzima <temirovaazima9@gmail.com>
2025-02-08 22:23:12 +0500

    proof of not rendering useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 export const App = ()=>{
     const [stateCount, setStateCount] = useState(0);
     const countRef = useRef(0);
+
+    useEffect(()=>{
+        console.log("Component re-rendered!")
+
+    })
     const incrementRef = ()=>{
         countRef.current+=1;
         // the reason u can see the updated result after pressing increase state is that because
@@ -14,6 +19,7 @@ export const App = ()=>{
     }
     return(
         <div>
+            {console.log("Rendering")}
             <p>State Count : {stateCount}</p>
             <p>countRef (Cant see) : {countRef.current}</p>
             <button onClick={incrementRef}>increase Ref</button>
